693d3deee20f457392ec5393d8ed0042a72032dc
Christopher Chee <cchee1@lsu.edu>
2020-04-19 23:01:03 -0500

    All stakeholder reports except 1 are working

---
+++ Web/appcount.php
@@ -0,0 +1,51 @@
+ <?php
+echo "<table style='border: solid 1px black;'>";
+echo "<tr><th>Company Name</th><th>Number of Applicants</th></tr>";
+
+class TableRows extends RecursiveIteratorIterator {
+    function __construct($it) {
+        parent::__construct($it, self::LEAVES_ONLY);
+    }
+
+    function current() {
+        return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
+    }
+
+    function beginChildren() {
+        echo "<tr>";
+    }
+
+    function endChildren() {
+        echo "</tr>" . "\n";
+    }
+}
+
+$servername = "4330project.mysql.database.azure.com";
+$username = "ProjectAdmin@4330project";
+$password = "Csc4330project";
+$dbname = "csc4330project";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $conn->prepare("SELECT company.c_name, COUNT(company.id_number)
+FROM employee_applications
+LEFT JOIN employee ON employee_applications.employee_id = employee.employee_id
+LEFT JOIN company ON employee.id_number = company.id_number 
+GROUP BY company.c_name;");
+    $stmt->execute();
+
+    // set the resulting array to associative
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+        echo $v;
+    }
+}
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$conn = null;
+echo "</table>";
+echo "<br>";
+echo "<a href = 'MainPage.html'>Back to Job Search Page</a>"
+?>
---
+++ Web/apptocount.php
@@ -0,0 +1,52 @@
+ <?php
+echo "<table style='border: solid 1px black;'>";
+echo "<tr><th>Company Name</th><th>Number of Applicants</th></tr>";
+
+class TableRows extends RecursiveIteratorIterator {
+    function __construct($it) {
+        parent::__construct($it, self::LEAVES_ONLY);
+    }
+
+    function current() {
+        return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
+    }
+
+    function beginChildren() {
+        echo "<tr>";
+    }
+
+    function endChildren() {
+        echo "</tr>" . "\n";
+    }
+}
+
+$servername = "4330project.mysql.database.azure.com";
+$username = "ProjectAdmin@4330project";
+$password = "Csc4330project";
+$dbname = "csc4330project";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $conn->prepare("SELECT company.c_name, COUNT(employee_applications.employee_id)
+FROM employee_applications
+LEFT JOIN job_opening ON employee_applications.job_id = job_opening.opening_id
+LEFT JOIN company ON job_opening.company_id = company.id_number 
+GROUP BY company.c_name
+ORDER BY COUNT(employee_applications.employee_id) DESC;");
+    $stmt->execute();
+
+    // set the resulting array to associative
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+        echo $v;
+    }
+}
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$conn = null;
+echo "</table>";
+echo "<br>";
+echo "<a href = 'MainPage.html'>Back to Job Search Page</a>"
+?>
